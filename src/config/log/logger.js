c2120c68ff8915c9641a68131a6b3ca24d629579
revzik <b.piekarz1997@gmail.com>
2021-04-18 16:23:21 +0200

    Add file transport

--- src/config/log/logger.js
+++ src/config/log/logger.js
@@ -1,12 +1,25 @@
 // logger config for separate modules
 const winston = require('winston');
+require('winston-daily-rotate-file');
 //const fileLoggerConfig = require(__dirname + '/config/log/winston.json');
+
+const LOG_DIR = __dirname + '../../../../logs';
 
 function createLogger(namespace) {
     const loggerConfig = {
         'transports': [
             new winston.transports.Console({
                 level: 'debug'
+            }),
+            new winston.transports.DailyRotateFile({
+                filename: 'bot-%DATE%.log',
+                datePattern: 'YYYY-MM-DD-HH',
+                zippedArchive: true,
+                maxSize: '20m',
+                maxFiles: '14d',
+                dirname: LOG_DIR,
+                createSymlink: true,
+                symlinkName: 'bot.log'
             })
         ],
         'format': winston.format.combine(
